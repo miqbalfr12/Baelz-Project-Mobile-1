275e297880643910609129ebcdcd50be0110448a
miqbalfr12 <miqbalfr12@gmail.com>
2024-07-08 22:24:12 +0700

    feat: edit data for product, sales, stock

---
+++ lib/pages/edit/edit_sales.dart
@@ -0,0 +1,337 @@
+import 'package:flutter/material.dart';
+import 'package:uas/modal/modal_content.dart';
+import 'package:uas/models/sales_model.dart';
+
+class EditSales extends StatefulWidget {
+  const EditSales({super.key, required this.sales});
+
+  final Sales sales;
+
+  @override
+  EditSalesState createState() => EditSalesState();
+}
+
+class EditSalesState extends State<EditSales> {
+  final _formKey = GlobalKey<FormState>();
+  String _itemBuyer = "";
+  String _itemStatus = "";
+  String _itemPhone = "";
+  String _itemDate = "";
+  DateTime? _selectedDate;
+  String? _selectedStatus;
+  final List<String> _statusOptions = [
+    'Done',
+    'Cicil',
+    'Belum Bayar',
+    'Pending',
+    'Cancel'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedStatus = widget.sales.status;
+    _dateController.text = widget.sales.date;
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.parse(widget.sales.date),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null && picked != _selectedDate) {
+      setState(() {
+        _selectedDate = picked;
+        _itemDate = "${picked.toLocal()}".split(' ')[0]; // Format as yyyy-mm-dd
+        _dateController.text = _itemDate;
+      });
+    }
+  }
+
+  final TextEditingController _dateController = TextEditingController();
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: const Text('Edit Sales'),
+      ),
+      backgroundColor: Colors.amber, // Background color of the main Scaffold
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity, // Set width to full width available
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: formEditSales(),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        print("Editing Sales");
+                        print({
+                          "buyer": _itemBuyer,
+                          "phone": _itemPhone,
+                          "status": _itemStatus,
+                          "date": _itemDate
+                        });
+                        showModalBottomSheet(
+                          context: context,
+                          backgroundColor: Colors.amber,
+                          builder: (BuildContext context) {
+                            return ModalContent(title: 'Edit Sales', obj: {
+                              "id": widget.sales.id,
+                              "buyer": _itemBuyer,
+                              "phone": _itemPhone,
+                              "status": _itemStatus,
+                              "date": _itemDate
+                            });
+                          },
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      shape: const StadiumBorder(),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
+                    ),
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.add, color: Colors.black),
+                        SizedBox(width: 8),
+                        Text(
+                          'Edit',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey.shade800,
+                      shape: const StadiumBorder(),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
+                    ),
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.close, color: Colors.amber),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Form formEditSales() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            keyboardType: TextInputType.text,
+            cursorColor: Colors.amber,
+            initialValue: widget.sales.buyer,
+            decoration: InputDecoration(
+              labelText: "Nama Buyer",
+              prefixIcon: const Icon(
+                Icons.assignment_ind_rounded, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Nama Buyer tidak boleh kosong";
+              }
+              return null;
+            },
+            onSaved: (value) => _itemBuyer = value!,
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            keyboardType: TextInputType.number,
+            cursorColor: Colors.amber,
+            initialValue: widget.sales.phone,
+            decoration: InputDecoration(
+              labelText: "Nomor Buyer",
+              prefixIcon: const Icon(
+                Icons.phone, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Nomor buyer tidak boleh kosong";
+              }
+              return null;
+            },
+            onSaved: (value) => _itemPhone = value!,
+          ),
+          const SizedBox(height: 10),
+          DropdownButtonFormField<String>(
+            value: _selectedStatus,
+            style: TextStyle(color: Colors.grey.shade700),
+            decoration: InputDecoration(
+              labelText: "Status",
+              prefixIcon: const Icon(
+                Icons.description, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            items: _statusOptions.map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Center(child: Text(value)),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                _selectedStatus = newValue;
+              });
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Status tidak boleh kosong";
+              }
+              return null;
+            },
+            onSaved: (value) => _itemStatus = value!,
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            controller: _dateController,
+            style: TextStyle(color: Colors.grey.shade700),
+            readOnly: true, // Prevent keyboard from appearing
+            onTap: () => _selectDate(context),
+            cursorColor: Colors.amber,
+            decoration: InputDecoration(
+              labelText: "Tanggal",
+              prefixIcon: const Icon(
+                Icons.date_range_rounded, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Tanggal tidak boleh kosong";
+              }
+              return null;
+            },
+            onSaved: (value) => _itemDate = value!,
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/edit/edit_stock.dart
@@ -0,0 +1,319 @@
+import 'package:flutter/material.dart';
+import 'package:uas/modal/modal_content.dart';
+
+import 'package:image_input/image_input.dart';
+import 'package:uas/models/stock_model.dart';
+
+class EditStock extends StatefulWidget {
+  const EditStock({super.key, required this.stock});
+
+  final Stock stock;
+
+  @override
+  EditStockState createState() => EditStockState();
+}
+
+class EditStockState extends State<EditStock> {
+  final _formKey = GlobalKey<FormState>();
+  String _itemName = "";
+  int _itemQty = 0;
+  String _itemAttr = "";
+  num _itemWeight = 0;
+  final List<XFile> _imageInputImages = [];
+
+  @override
+  Widget build(BuildContext context) {
+    print(widget.stock);
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        title: const Text('Edit Stock'),
+      ),
+      backgroundColor: Colors.amber, // Background color of the main Scaffold
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity, // Set width to full width available
+            height: MediaQuery.of(context).size.height,
+            decoration: const BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40),
+                topRight: Radius.circular(40),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: formEditStock(),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              alignment: Alignment.bottomCenter,
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState!.save();
+                        print({
+                          "name": _itemName,
+                          "attr": _itemAttr,
+                          "qty": _itemQty,
+                          "weight": _itemWeight,
+                          "id": widget.stock.id,
+                          "image": _imageInputImages
+                        });
+                        showModalBottomSheet(
+                          context: context,
+                          backgroundColor: Colors.amber,
+                          builder: (BuildContext context) {
+                            return ModalContent(title: 'Edit Stock', obj: {
+                              "name": _itemName,
+                              "attr": _itemAttr,
+                              "qty": _itemQty,
+                              "weight": _itemWeight,
+                              "id": widget.stock.id,
+                              "image": _imageInputImages
+                            });
+                          },
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber,
+                      shape: const StadiumBorder(),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
+                    ),
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.edit, color: Colors.black),
+                        SizedBox(width: 8),
+                        Text(
+                          'Edit',
+                          style: TextStyle(fontSize: 16, color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey.shade800,
+                      shape: const StadiumBorder(),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12),
+                    ),
+                    child: const Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(Icons.close, color: Colors.amber),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Form formEditStock() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          ImageInput(
+            images: _imageInputImages,
+            imageContainerDecoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.amber,
+                width: 2.0,
+              ),
+            ),
+            addImageContainerDecoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(100.0),
+            ),
+            allowEdit: true,
+            allowMaxImage: 1,
+            onImageSelected: (image) {
+              setState(() {
+                _imageInputImages.add(image);
+              });
+            },
+            onImageRemoved: (image, index) {
+              setState(() {
+                _imageInputImages.remove(image);
+              });
+            },
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            keyboardType: TextInputType.text,
+            cursorColor: Colors.amber,
+            decoration: InputDecoration(
+              labelText: "Nama Barang",
+              prefixIcon: const Icon(
+                Icons.shopping_bag_outlined, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Nama barang tidak boleh kosong";
+              }
+              return null;
+            },
+            initialValue: widget.stock.name,
+            onSaved: (value) => _itemName = value!,
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            keyboardType: TextInputType.number,
+            cursorColor: Colors.amber,
+            decoration: InputDecoration(
+              labelText: "Quantity",
+              prefixIcon: const Icon(
+                Icons.view_in_ar_outlined, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Quantity tidak boleh kosong";
+              }
+              return null;
+            },
+            initialValue: widget.stock.qty.toString(),
+            onSaved: (value) => _itemQty = int.parse(value!),
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            keyboardType: TextInputType.text,
+            cursorColor: Colors.amber,
+            decoration: InputDecoration(
+              labelText: "Attribute",
+              prefixIcon: const Icon(
+                Icons.category_outlined, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Attribute tidak boleh kosong";
+              }
+              return null;
+            },
+            initialValue: widget.stock.attr,
+            onSaved: (value) => _itemAttr = value!,
+          ),
+          const SizedBox(height: 10),
+          TextFormField(
+            style: TextStyle(color: Colors.grey.shade700),
+            keyboardType: TextInputType.number,
+            cursorColor: Colors.amber,
+            decoration: InputDecoration(
+              labelText: "Weight",
+              prefixIcon: const Icon(
+                Icons.scale_rounded, // Change to your desired icon
+              ),
+              focusColor: Colors.amber,
+              prefixIconColor: Colors.grey.shade700,
+              floatingLabelStyle: const TextStyle(
+                color: Colors.amber,
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0),
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(100.0), // Set border radius
+                borderSide: const BorderSide(
+                  color: Colors.amber, // Set border color
+                ),
+              ),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return "Weight tidak boleh kosong";
+              }
+              return null;
+            },
+            initialValue: widget.stock.weight.toString(),
+            onSaved: (value) => _itemWeight = num.parse(value!),
+          ),
+        ],
+      ),
+    );
+  }
+}

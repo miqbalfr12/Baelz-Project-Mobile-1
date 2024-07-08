9d641abe11438d2f6aeafd2dc13dae61ea79f9b6
miqbalfr12 <miqbalfr12@gmail.com>
2024-07-08 22:30:53 +0700

    add: permission_handler

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,35 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:uas/widget/main_wrapper.dart';
 import 'package:uas/bloc/bottom_nav_cubit.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    requestPermissions();
+  }
+
+  Future<void> requestPermissions() async {
+    var status = await Permission.camera.status;
+    if (!status.isGranted) {
+      await Permission.camera.request();
+    }
+
+    status = await Permission.storage.status;
+    if (!status.isGranted) {
+      await Permission.storage.request();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
